bf713eb66045d86386ea2d200863ed6a9cc8cd63
David <throwasix@gmail.com>
2018-07-20 14:23:11 +0100

    Mongo inserts added

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,11 +1,70 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:adminRoutes');
 
 const adminRouter = express.Router();
+
+const books = [
+  {
+    title: 'Mort',
+    genre: 'Fantasy',
+    author: 'Terry Pratchett',
+    read: true
+  },
+  {
+    title: 'Papillon',
+    genre: 'Crime',
+    author: 'Henri Charrierre',
+    read: true
+  },
+  {
+    title: 'The Time Machine',
+    genre: 'Science Fiction',
+    author: 'H. G. Wells',
+    read: false
+  },
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  },
+  {
+    title: 'Les Miserables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    read: false
+  },
+  {
+    title: 'Thud',
+    genre: 'Fantasy',
+    author: 'Terry Pratchett',
+    read: true
+  },
+];
 
 function router(nav) {
   adminRouter.route('/')
     .get((req, res) => {
-      res.send('inserting books');
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected to the server');
+
+          const db = client.db(dbName);
+
+          const response = await db.collection('books').insertMany(books);
+          res.json(response);
+        } catch (err) {
+          debug(err.stack);
+        }
+
+        client.close();
+      }());
     });
 
   return adminRouter;
